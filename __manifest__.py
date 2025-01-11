8492df9b963f6f225d8d03f1fb71603e74ac8de8
aldyahsn <aldy.ahsandin@gmail.com>
2025-01-11 12:58:30 +0700

    add script to ignore default alert

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'KSO: Allow Fraction Quantity',
+    'version': '1.0',
+    'description': '',
+    'summary': '',
+    'author': '',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': '',
+    'depends': [
+        'base','product', 'stock',
+    ],
+    # 'data': [
+    #     ''
+    # ],
+    # 'demo': [
+    #     ''
+    # ],
+    'auto_install': False,
+    'application': False,
+    # 'assets': {
+        
+    # }
+}
